67519ba0589b42d6227069abfe7c0244f59d5077
Oliver <ocben1@gmail.com>
2021-02-15 07:44:29 +1100

    fetch api demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+  const getPost = () => fetch(`https://jsonplaceholder.typicode.com/posts/4`).then(response => response.json()); 
 
 function App() {
+  //post is the current state, updatePosts is the function that update the value of the first variable, which is our current state
+  const [post, updatePosts] = useState(null);
+//fetches data and updates the state which causes a re-render, component renders again and useEffect is called once more. Cycle continues
+  useEffect(() => {
+      (async () => {//need to place sync functions inside body of useEffect - effect mustn't return anything else besides function. By making the function async, it will return a Promise
+          updatePosts(await getPost());
+      })();
+  }, []);
+
+  if (!post) {
+      return <div>Loading...</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <header className="App-header">
+
+              <h2>{post.title}</h2>
+          </header>
+          <main>{post.body}</main>
+          
+      </div>
   );
 }
 
